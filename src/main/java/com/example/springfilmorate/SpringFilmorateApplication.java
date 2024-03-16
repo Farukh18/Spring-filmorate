eb52acefca076eb991295879f829ba4bfbea69b0
Farukh <excommunicad00@yandex.ru>
2024-03-16 15:22:53 +0600

    first commit

---
+++ src/main/java/com/example/springfilmorate/SpringFilmorateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfilmorate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFilmorateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFilmorateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springfilmorate/SpringFilmorateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfilmorate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFilmorateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
